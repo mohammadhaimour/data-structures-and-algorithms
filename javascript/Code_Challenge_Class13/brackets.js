3aac0d038585335e6e272197c62da3d30168b8c7
mohammadhaimour <mohammadhaimour1999@gmail.com>
2022-06-29 07:29:18 +0300

    cha13

---
+++ javascript/Code_Challenge_Class13/brackets.js
@@ -0,0 +1,68 @@
+'use strict';
+function validatebrackets(str) {
+    let arr = str.split('');
+    let arr2 = [];
+
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] === '{' || arr[i] === '}' || arr[i] === '(' || arr[i] === ')' || arr[i] === '[' || arr[i] === ']')
+            arr2.push(arr[i]);
+    }
+    //console.log(arr2);
+
+
+
+
+    let OpenRoundBrackets = 0;//(
+    let ClosetRoundBrackets = 0;//)
+
+    let OpenSquareBrackets = 0;//[
+    let ClosetSquareBrackets = 0;//]
+
+
+    let OpenCurlyBrackets = 0;//{
+    let ClosepenCurlyBrackets = 0;//{
+
+
+    arr2.forEach((bra) => {
+        if (bra === '(')
+            OpenRoundBrackets++;
+
+        else if (bra === ')')
+            ClosetRoundBrackets++;
+
+        else if (bra === '[')
+            OpenSquareBrackets++;
+
+        else if (bra === ']')
+            ClosetSquareBrackets++;
+
+        else if (bra === '{')
+            OpenCurlyBrackets++;
+
+        else if (bra === '}')
+            ClosepenCurlyBrackets++;
+
+
+    });
+
+    if (OpenRoundBrackets === ClosetRoundBrackets &&
+        OpenSquareBrackets === ClosetSquareBrackets &&
+        OpenCurlyBrackets === ClosepenCurlyBrackets)
+        return true;
+
+    else
+        return false;
+
+}
+module.exports = validatebrackets;
+// console.log(brackets('{}'));//TRUE
+// console.log(brackets('{}[][]'));//TRUE
+// console.log(brackets('()[[Extra Characters]]'));//TRUE
+// console.log(brackets('(){}[[]]'));//TRUE
+// console.log(brackets('[({}]'));//false
+// console.log(brackets('(]('));//false
+
+
+
+
+
